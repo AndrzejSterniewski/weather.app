160d8411922117a74e2e23e6ae943a9c455ec55b
Andrzej Sterniewski <drapiszon@gmail.com>
2023-11-19 22:42:24 +0100

    initial commit

---
+++ src/components/WeatherBox/WeatherBox.js
@@ -0,0 +1,21 @@
+import PickCity from '../PickCity/PickCity';
+import WeatherSummary from '../WeatherSummary/WeatherSummary';
+import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
+
+const WeatherBox = () => {
+
+  const handleCityChange = useCallback(city => {
+    console.log('city', city);
+  }, []);
+
+  return (
+    <section>
+      <PickCity action={handleCityChange} />
+      <WeatherSummary />
+      <Loader />
+    </section>
+  )
+};
+
+export default WeatherBox;
